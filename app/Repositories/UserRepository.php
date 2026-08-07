e5a62be82b2773bc89e7f0f098371237720a3b16
Dimitri Nelson Baliguini Demba <ton-email-github@exemple.com>
2026-08-07 12:53:35 +0100

    feat: architecture Repository pour User, packages 2FA installes

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\Contracts\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function findByEmail(string $email): ?User
+    {
+        return User::where('email', $email)->first();
+    }
+
+    public function findById(int $id): ?User
+    {
+        return User::find($id);
+    }
+
+    public function update(User $user, array $data): User
+    {
+        $user->update($data);
+
+        return $user->fresh();
+    }
+}
